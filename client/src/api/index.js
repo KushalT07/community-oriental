4f24f66e51c98bc174e36f2c737a6e8210c3041e
KushalT07 <82999062+KushalT07@users.noreply.github.com>
2023-04-12 23:19:32 +0530

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000' });
+const API = axios.create({ baseURL: 'https://kushalt07.github.io/community-oriental/' });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
